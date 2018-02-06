0b8357fffc07027da67e4f8b684c0a68344573fd
Matt Clarkson <matthew.clarkson@ef.com>
2018-02-06 13:31:10 +0000

    feat(export): expose `File` and `IFileOptions`

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,7 @@
+import File from '@lib/File';
+export { File };
+export { IOptions as IFileOptions } from '@lib/File';
+
 import convert from '@lib/convert';
 export * from '@lib/convert';
 export default convert;
